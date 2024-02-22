8dfbbfcaa5a3d5a63109d03fc7cbb150f3bde75d
Juan Pablo Rivera <jpriveram01@gmail.com>
2024-02-22 10:08:32 -0400

    Se agrego funcionalidad para imprimir un numero sin regla como el 1

---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,11 @@
+//import generarfizzbuzz from "./fizzbuzz.js";
+
+describe("Sumar", () => {
+  it("Deberia generar un numero sin regla como el 1", () => {
+    expect(generarfizzbuzz(1)).toEqual("1");
+  });
+});
+
+function generarfizzbuzz(n){
+    return "1";
+}
